17731bafb061a17cf17e75f0d14dcc0849e51d6c
apumjp0508 <akihisatengshan@gmail.com>
2025-01-09 22:17:52 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const getBtn=document.getElementById("btn");
+const text=document.getElementById("text");
+
+getBtn.addEventListener("click",()=>{
+    setTimeout(() => {
+        text.textContent="ボタンをクリックしました";
+    }, 2000);
+});
