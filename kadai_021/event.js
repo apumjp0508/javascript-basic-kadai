82b93f0d49c2b7c7df8dc0cdd7a5e4a99742787a
apumjp0508 <akihisatengshan@gmail.com>
2025-01-09 22:17:30 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,6 +0,0 @@
-const getBtn=document.getElementById("btn");
-const text=document.getElementById("text");
-
-getBtn.addEventListener("click",setTimeout(()=>{
-    text.textContent="ボタンをクリックしました";
-},2000));
